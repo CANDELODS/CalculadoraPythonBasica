5d20a45c66c4ca3c5af19f033fb2def926d766df
Juan Sebastian Candelo Balanta <jcandelobalanta@gmail.com>
2024-06-07 09:17:55 -0500

    first commit

---
+++ myCalculator.py
@@ -0,0 +1,10 @@
+#Calculadora Desarrollada Por Juan Candelo
+while True:
+     print("""Opciones
+    Ingrese '+' para sumar dos numeros
+    Ingrese '-' para restar dos numeros
+    Ingrese '*' para multiplicar dos numeros
+    Ingrese '/' para dividir dos numeros
+    Ingrese 'salir' para finalizar""")
+
+    #teclado=input(":")
